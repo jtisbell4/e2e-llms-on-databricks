4b7b5f0baebf33ccb648476aadcb04afdbfc5dd0
Taylor Isbell <taylor.isbell@databricks.com>
2023-07-21 16:08:25 -0400

    set DATABRICKS_HOST

--- serve_model.py
+++ serve_model.py
@@ -4,19 +4,21 @@ from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.serving import EndpointCoreConfigInput
 from mlflow import MlflowClient
 
+os.environ["DATABRICKS_TOKEN"] = dbutils.secrets.get(
+    scope="jtisbell", key="db-token"
+)
+os.environ["DATABRICKS_HOST"] = "https://e2-demo-field-eng.cloud.databricks.com"
+
+w = WorkspaceClient()
+mlflow_client = MlflowClient()
+
 # TODO: make dynamic
 model_name = "llamav2_7b_chat_model"
 endpoint_name = "llama2-7b-chat"
 
-mlflow_client = MlflowClient()
 model_version = mlflow_client.get_latest_versions(
     name=model_name, stages=["Staging"]
 )[0]
-
-os.environ["DATABRICKS_TOKEN"] = dbutils.secrets.get(
-    scope="jtisbell", key="db-token"
-)
-w = WorkspaceClient(auth_type="pat")
 
 endpoint_config = {
     "name": endpoint_name,
