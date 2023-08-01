009e925691de2e252cbfa6388be9b56f79e83654
Taylor Isbell <taylor.isbell@databricks.com>
2023-08-01 14:59:26 -0400

    formatting and added model.py

--- chatbot.py
+++ chatbot.py
@@ -8,21 +8,28 @@ If a question does not make any sense, or is not factually coherent, explain why
 
 class Chatbot:
     def __init__(self):
-        self.conversation_history = ""
+        self.conversation_list = [
+            f"<s>[INST]<<SYS>>\n{DEFAULT_SYSTEM_PROMPT}\n<</SYS>>\n\n"
+        ]
         self.model = Databricks(
             endpoint_name="llama2-7b-chat",
-            model_kwargs={"temperature": 0.1, "max_new_tokens": 500},
+            model_kwargs={"temperature": 0.1, "max_new_tokens": 4000},
         )
 
-    def dummy_response(self):
-        return "I'm just a dumb chatbot right now."
-
     def chat(self, instruction):
-        self.conversation_history += instruction + " [/INST] "
-        if len(self.conversation_history) > 4000:
-            cropped_history = ...
-        response = self.model(self.conversation_history)
-        self.conversation_history += response + " </s><s> "
+        # Truncate converation history to roughly 2500 words
+        while sum([len(x.split(" ")) for x in self.conversation_list]) > 2500:
+            self.conversation_list.pop(1)
+
+        conversation = "".join(self.conversation_list)
+        conversation += instruction + "[/INST]"
+
+        response = self.model(conversation)
+
+        self.conversation_list.append(
+            instruction + "[/INST]" + response + "</s><s>"
+        )
+
         return response
 
 
