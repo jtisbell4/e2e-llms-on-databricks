d48241644b4606039f2e612672ffd8b3ec968709
Taylor Isbell <taylor.isbell@databricks.com>
2023-08-23 21:05:06 -0400

    added memory

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # End-to-end LLM Deployments in Databricks 😎
+
+Just messing around with different ways of quickly deploying and interacting with LLMs served on Databricks...
+
+A couple things:
+* This POC uses Langchain, so you can quickly swap out the LLM you wish to use by redefining 
--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import time
 
 import gradio as gr
-from langchain.llms import Databricks
 
-system_message = {"role": "system", "content": "You are a helpful assistant."}
-llm = Databricks(endpoint_name="llama2-7b-chat")
+from langchain_bot import chat_llm_chain
 
 with gr.Blocks() as demo:
     chatbot = gr.Chatbot()
@@ -18,23 +16,28 @@ with gr.Blocks() as demo:
 
     def bot(history, messages_history):
         user_message = history[-1][0]
-        bot_message, messages_history = ask_gpt(user_message, messages_history)
+
+        bot_message, messages_history = get_response(
+            user_message, messages_history
+        )
+
         messages_history += [{"role": "assistant", "content": bot_message}]
+
         history[-1][1] = bot_message
+
         time.sleep(1)
         return history, messages_history
 
-    def ask_gpt(message, messages_history):
+    def get_response(message, messages_history):
         messages_history += [{"role": "user", "content": message}]
-        # response = openai.ChatCompletion.create(
-        #     model="gpt-3.5-turbo", messages=messages_history
-        # )
-        response = llm(message, temperature=0.1, max_new_tokens=200)
-        return response["choices"][0]["message"]["content"], messages_history
+
+        response = chat_llm_chain.predict(human_input=message)
+
+        return response, messages_history
 
     def init_history(messages_history):
         messages_history = []
-        messages_history += [system_message]
+        # messages_history += [system_message]
         return messages_history
 
     msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
